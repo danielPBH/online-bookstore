1f7293c4563373583fb7fbdf62abce83a569f1cb
danielb <dbarreto@soaint.com>
2020-06-11 11:14:13 +0200

    commit 3

---
+++ angular-bookstore/src/app/components/book-list/book-list.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Book } from '../../common/book';
+import { BookService } from 'src/app/services/book.service';
+
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.css']
+})
+export class BookListComponent implements OnInit {
+
+  books: Book[] = [];
+
+  constructor(private _bookService: BookService) { }
+
+  ngOnInit(): void {
+    this.listBooks();
+  }
+
+  listBooks(){
+    // this.searchMode = this._activatedRoute.snapshot.paramMap.has('keyword');
+
+    // if(this.searchMode){
+    //   //do search work
+    //   this.handleSearchBooks();
+    // }else {
+    //   //display books based on category
+    //   this.handleListBooks();
+    // }
+
+    this._bookService.getBooks().subscribe(
+      data => this.books = data
+    )
+  }
+
+}
--- onlinebookstore/src/main/java/com/example/onlinebookstore/repository/BookRepository.java
+++ onlinebookstore/src/main/java/com/example/onlinebookstore/repository/BookRepository.java
@@ -1,9 +1,12 @@
 package com.example.onlinebookstore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.example.onlinebookstore.entity.Book;
 
+//@CrossOrigin("*")
+@CrossOrigin("http://localhost:4200")
 public interface BookRepository extends JpaRepository<Book,Long>{
 
 }
